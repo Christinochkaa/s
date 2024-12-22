678b6b245597d597328d99a8a089704e85401ff5
Christinochkaa <146668828+Christinochkaa@users.noreply.github.com>
2024-12-23 00:49:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество цифр:");
+        int count = int.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++)
+        {
+            Process.Start("notepad.exe");
+        }
+        Console.ReadLine();
+        Kill();
+    }
+    static void Kill()
+    {
+        Process[] notepads = Process.GetProcessesByName("notepad");
+
+        foreach (Process notepad in notepads)
+        {
+            notepad.Kill();
+        }
+        Console.WriteLine("Все процессы Notepad были завершены.");
+    }
+}
